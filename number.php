56c56c51d8d90451f9dc21e1a1aedb9f1a337ea7
devsh <digimon.1740@gmail.com>
2017-12-20 23:34:54 +1100

    init

---
+++ number.php
@@ -0,0 +1,5 @@
+<?php
+//	echo 1;
+//	echo 4 / 3;
+	//var_dump(1);
+?>
---
+++ string.php
@@ -0,0 +1,11 @@
+<?php
+	//	echo "Hello world!";
+	//	var_dump(123);
+	//	var_dump("123");
+	echo "Hello" . " " . "World";
+	echo "  ";
+	echo "Hello\" Sanghoon \" World";
+	echo "  ";
+	echo 'Hello"Sanghoon "World';
+?>
+
